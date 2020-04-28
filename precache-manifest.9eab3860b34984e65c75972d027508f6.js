a47a81388a7f2a1687cc4034bca1edba595a64ce
‘peter-robert-cole’ <petercole3@gmail.com>
2020-04-28 16:19:50 -0700

    initial

---
+++ precache-manifest.9eab3860b34984e65c75972d027508f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a4aabca668bde1f4a245eae39b77c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f45855546f62d15c167",
+    "url": "/static/css/main.4fd7ad98.chunk.css"
+  },
+  {
+    "revision": "4bc7777c67562ad67493",
+    "url": "/static/js/2.b736e420.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b736e420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f45855546f62d15c167",
+    "url": "/static/js/main.5f8d1fd7.chunk.js"
+  },
+  {
+    "revision": "668cfeceec6270a571c5",
+    "url": "/static/js/runtime-main.42aa0cb9.js"
+  }
+]);
