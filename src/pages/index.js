afd321b2c00c36cba8b2a920565c1af67b5e84e3
peter-robert-cole <peter7cole@gmail.com>
2022-03-08 02:25:31 -0800

    feat: added font

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,10 @@ const IndexPage = () => {
 
   return (
     <>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;1,200;1,400&display=swap');
+      </style>
       <div className="diagonal"></div>
       <div className="box1">
         <div className="box2">
